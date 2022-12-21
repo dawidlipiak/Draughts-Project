3e275c9ad18b6f3925023c7099e0fe86cd75302f
dawidlipiak <dawidlipiak@gmail.com>
2022-12-21 16:42:00 +0100

    Added class Pawn and Player. Added for Player class methods for setting position of pawns. Player's methods are in progress. Changes to Board class - added functions to get center positions of fields.

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -1,40 +1,69 @@
 package org.example;
 
+import javafx.collections.ObservableList;
+import javafx.geometry.Point2D;
 import javafx.geometry.Pos;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 public class Board extends GridPane{
+    // Array of center points of every square field on board
+    private final Point2D[][] centerFieldPosition;
+    // Array of square fields
+    private final Rectangle[][] fields;
 
     /**
-     * Gridpane board for play
+     * Constructor of a play board for draughts
+     * @param numberOfFields number of row fields
+     * @param boardSize size of a window
      */
-    public Board(int numberOfFields){
-        int boardSize = 600;
+    public Board(int numberOfFields,int boardSize){
         int count = 0;
+        fields = new Rectangle[numberOfFields][numberOfFields];
+        centerFieldPosition = new Point2D[numberOfFields][numberOfFields];
 
         // side of rectangle
-        int s = boardSize / numberOfFields;
+        int side = boardSize / numberOfFields;
 
         // Create 64 rectangles and add to gridPane
         for (int i = 0; i < numberOfFields; i++) {
             count++;
             for (int j = 0; j < numberOfFields; j++) {
-                Rectangle rectangle = new Rectangle(s, s, s, s);
-                if (count % 2 == 0)
+                Rectangle rectangle = new Rectangle(side, side, side, side);
+                if (count % 2 == 0) {
                     rectangle.setFill(Color.WHITE);
-                this.add(rectangle, j, i);
+                    fields [i][j] = rectangle;
+                    //System.out.println(fields[i][j]);
+                }
+                add(rectangle, j, i);
                 count++;
             }
         }
         setAlignment(Pos.CENTER);
+        //setCenterFieldPosition();
     }
 
-//   ? DO KONSULTACJI  ?
-//    public GridPane getBoard() {
-//        return this;
-//    }
+    /**
+     * Function for setting center positions of fields
+     */
+    private void setCenterFieldPosition () {
+
+        for(int i = 0; i < getRowCount(); i++){
+            for(int j = 0; j < getColumnCount(); j++){
+                Point2D point = new Point2D(fields[i][j].getX(),fields[i][j].getY());
+                centerFieldPosition[i][j] = point;
+            }
+        }
+    }
+
+    /**
+     * Function returning points of fields' center positions
+     * @param row of filed
+     * @param column of field
+     * @return point of center position for given row and column
+     */
+    public Point2D getCenterFieldPosition (int row, int column) {
+        return centerFieldPosition[row][column];
+    }
 }
